2ce12914631b2be405221ed2b051efc0006eae75
Unknown15082 <trangiahuy15082006@gmail.com>
2024-12-04 19:13:55 +0800

    Fix cmp.nix

--- config/core/cmp.nix
+++ config/core/cmp.nix
@@ -19,14 +19,12 @@
 			enable = true;
 			autoEnableSources = true;
 			settings = {
-				preselect = "cmp.PreselectMode.None";
 				snippet.expand = "function(args) require('luasnip').lsp_expand(args.body) end";
 
 				sources = [
 					{ name = "nvim_lsp"; priority = 1000; }
 					{ name = "nvim_lsp_signature_help"; priority = 1000; }
 					{ name = "nvim_lsp_document_symbol"; priority = 1000; }
-					{ name = "treesitter"; priority = 850; }
 					{ name = "luasnip"; priority = 750; }
 					{ name = "buffer"; priority = 500; }
 					{ name = "rg"; priority = 300; }
@@ -45,6 +43,14 @@
 					"<C-f>" = "cmp.mapping.scroll_docs(4)";
 					"<C-b>" = "cmp.mapping.scroll_docs(-4)";
 					"<CR>" = "cmp.mapping.confirm({ select = true; })";
+					"<Tab>" = ''
+					cmp.mapping(function(fallback)
+						if require('luasnip').expand_or_jumpable() then
+							require('luasnip').expand_or_jump()
+						else
+							fallback()
+						end
+					end, {'i', 's'})'';
 				};
 			};
 		};
